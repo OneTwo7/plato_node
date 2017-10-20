4c98b74bc565e1fc9883f14f2d0a1f2d7904d450
onetwo7 <lahoohal@gmail.com>
2017-10-20 14:39:49 +0300

    Added password confirmation

---
+++ public/js/directives/mvPasswordConfirmation.js
@@ -0,0 +1,19 @@
+angular.module('app').directive('mvPasswordConfirmation', function () {
+  return {
+    require: 'ngModel',
+    scope: {
+      passwordValue: '=mvPasswordConfirmation'
+    },
+    link: function (scope, element, attributes, ngModel) {
+
+      ngModel.$validators.mvPasswordConfirmation = function (modelValue) {
+          return scope.passwordValue && scope.passwordValue.length === 0 || modelValue === scope.passwordValue;
+      };
+
+      scope.$watch('passwordValue', function () {
+          ngModel.$validate();
+      });
+
+    }
+  };
+});
