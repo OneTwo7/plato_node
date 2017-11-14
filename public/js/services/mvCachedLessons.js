4e498e049f9789a795f279f3745540b627baa6c8
onetwo7 <lahoohal@gmail.com>
2017-11-14 09:35:47 +0300

    Fixed Cached Lessons

--- public/js/services/mvCachedLessons.js
+++ public/js/services/mvCachedLessons.js
@@ -1,9 +1,11 @@
 angular.module('app').factory('mvCachedLessons', function (mvLesson) {
+  var courseId;
   var lessonList;
 
   return {
     query: function (id) {
-      if (!lessonList) {
+      if (!lessonList || courseId !== id) {
+        courseId = id;
         lessonList = mvLesson.get({ _id: id });
       }
       return lessonList;
